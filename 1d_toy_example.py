bb20234c30a73d3dfa23da97e7f69b8426cfe8d2
Ege şirin <32102094+egesirin@users.noreply.github.com>
2022-04-18 15:15:08 +0200

    smooth partition

--- 1d_toy_example.py
+++ 1d_toy_example.py
@@ -44,9 +44,9 @@ def gt(dim):
 
 
 def from_funct_to_matrix_cov_is_diagonal(mean_func, log_diagonal_func, dim):
-    #epsilon = 1e-4
+    epsilon = 1e-4
     #index = torch.arange(-dim//2, dim//2).to(torch.float32)*epsilon
-    index = torch.arange(dim).to(torch.float32)
+    index = torch.arange(dim).to(torch.float32)*epsilon
     index = index.unsqueeze(1)
     mean_vec = mean_func(index)
     diagonal_matrix = torch.diag(torch.exp(log_diagonal_func(index).view(-1)))
@@ -54,9 +54,9 @@ def from_funct_to_matrix_cov_is_diagonal(mean_func, log_diagonal_func, dim):
 
 
 def from_funct_to_matrix_cov_is_low_rank(mean_func, log_diagonal_func, cov_factor_func, dim):
-    #epsilon = 1e-4
+    epsilon = 1e-4
     #index = torch.arange(-dim//2, dim//2).to(torch.float32)*epsilon
-    index = torch.arange(dim).to(torch.float32)
+    index = torch.arange(dim).to(torch.float32)*epsilon
     index = index.unsqueeze(1)
     mean_vector = mean_func(index)
     diagonal_matrix = torch.diag(torch.exp(log_diagonal_func(index).view(-1)))
@@ -106,7 +106,7 @@ class DeepSDF(nn.Module):
         self.fc1 = nn.Linear(in_ch + latent_size, 32)
         self.fc2 = nn.Linear(32, 32)
         self.fc3 = nn.Linear(32, 32)
-        self.fc4 = nn.Linear(32, 32 - latent_size)
+        self.fc4 = nn.Linear(32, 32 - (in_ch+latent_size))
         self.fc5 = nn.Linear(32, 32)
         self.fc6 = nn.Linear(32, 32)
         self.fc7 = nn.Linear(32, 32)
@@ -118,7 +118,7 @@ class DeepSDF(nn.Module):
         x3 = F.relu(self.fc2(x2))
         x4 = F.relu(self.fc3(x3))
         x5 = F.relu(self.fc4(x4))
-        y = torch.cat((self.latent_parameter, x5), 1)
+        y = torch.cat((x1, x5), 1)
         x6 = F.relu(self.fc5(y))
         x7 = F.relu(self.fc6(x6))
         x8 = F.relu(self.fc7(x7))
@@ -161,17 +161,17 @@ def train_low_rank(t, number_pre_epochs, mean, log_diagonal, cov_factor, loss_fu
         mean_vec, diagonal_vec, cov_factor_matrix = from_funct_to_matrix_cov_is_low_rank(mean, log_diagonal,
                                                                                          cov_factor, dim)
         mc_samples0 = mc_sample_mean(mean_vec, dim, number_of_samples)
-        #mc_samples1 = mc_sample_mean(mean_vec, dim, number_of_samples)
+        mc_samples1 = mc_sample_mean(mean_vec, dim, number_of_samples)
 
     else:
         optimizer = optimizer_a
         mean_vec, diagonal_vec, cov_factor_matrix = from_funct_to_matrix_cov_is_low_rank(mean, log_diagonal,
                                                                                          cov_factor, dim)
         mc_samples0 = mc_sample_cov_is_low_rank(mean_vec, diagonal_vec, cov_factor_matrix, number_of_samples)
-        #mc_samples1 = mc_sample_cov_is_low_rank(mean_vec, diagonal_vec, cov_factor_matrix, number_of_samples)
+        mc_samples1 = mc_sample_cov_is_low_rank(mean_vec, diagonal_vec, cov_factor_matrix, number_of_samples)
     for j in range(number_of_samples):
         log_prob[0][j] = -loss_function(mc_samples0[j], gts[0])
-        log_prob[1][j] = -loss_function(mc_samples0[j], gts[1])
+        log_prob[1][j] = -loss_function(mc_samples1[j], gts[1])
     loss = torch.mean(-torch.logsumexp(log_prob, dim=1) + math.log(number_of_samples))
     loss_list.append(loss.item())
     optimizer.zero_grad()
